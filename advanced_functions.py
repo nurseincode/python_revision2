88ca6cf7484fed4f09bdbdd58f3d90fa057441b2
https://github.com/nurseincode <nurseincode@gmail.com>
2026-08-30 21:31:17 +1000

    callback basics

---
+++ advanced_functions.py
@@ -0,0 +1,24 @@
+# advanced functions
+# 1. Callbacks
+
+# def greet():
+#     print(f'Hello!')
+
+
+# def doing_something(callback):
+#     print(f'Running first')
+#     callback()
+
+# doing_something(greet)
+
+
+def greet(name):
+    print(f'Hello, {name}!')
+
+def say_bye():
+    print('Goodbye!')
+   
+
+greet('Kare!')
+say_bye()
+
