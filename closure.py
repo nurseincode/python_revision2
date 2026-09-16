f318d678e8c3fc0adca5be7be8bc3aff3bb26634
https://github.com/nurseincode <nurseincode@gmail.com>
2026-09-16 21:20:30 +1000

    closure3

--- closure.py
+++ closure.py
@@ -2,14 +2,26 @@
 # a function that returns another function
 # The returned function retains a copy of the scope of the outer function
 
-def create_multiplier(x):
+# def create_multiplier(x):
     
-    def multiply(number):
-          return number * x
-    return multiply
+#     def multiply(number):
+#           return number * x
+#     return multiply
 
-double = create_multiplier(2)
-print(double(5))
+# double = create_multiplier(2)
+# print(double(5))
+
+def greet(name): 
+    print('Hello')
+
+    def display_name():
+        print(name)
+    return display_name
+
+spam = greet('Dee')
+
+     
+
 
 
   
