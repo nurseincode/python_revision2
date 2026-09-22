e1502f534da281c7a8c19977dfaa8e4f0e9a0c3f
https://github.com/nurseincode <nurseincode@gmail.com>
2026-09-22 22:09:12 +1000

    set methods

--- sets.py
+++ sets.py
@@ -1,6 +1,14 @@
 # Set - Unordered- Not indexed - mutable - no duplicates
 
-names_set = {'Dee, Tim, Kim', 'Lee'}
+names_set = {'Dee', 'Tim', 'Kim', 'Lee'}
 
+
+print(len(names_set))
+names_set.add('Mary')
+names_set.remove('Mary')
+names_set.add('Dee')
 print(names_set)
+
+print('Mary' in names_set)
+
 print(type(names_set))
