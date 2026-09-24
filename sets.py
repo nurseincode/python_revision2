69bdaa9e9a172df0311a7ec50342cea805227d4d
https://github.com/nurseincode <nurseincode@gmail.com>
2026-09-24 13:24:32 +1000

    set operations

--- sets.py
+++ sets.py
@@ -1,14 +1,22 @@
 # Set - Unordered- Not indexed - mutable - no duplicates
 
-names_set = {'Dee', 'Tim', 'Kim', 'Lee'}
+# names_set = {'Dee', 'Tim', 'Kim', 'Lee'}
 
 
-print(len(names_set))
-names_set.add('Mary')
-names_set.remove('Mary')
-names_set.add('Dee')
-print(names_set)
+# print(len(names_set))
+# names_set.add('Mary')
+# names_set.remove('Mary')
+# names_set.add('Dee')
+# print(names_set)
 
-print('Mary' in names_set)
+# print('Mary' in names_set)
 
-print(type(names_set))
+# print(type(names_set))
+
+set1 = {1, 2, 3, 4, 5}
+set2 = {1, 3, 5, 7, 9}
+
+print(set1.union(set2))
+print(set1.intersection(set2))
+print(set1.difference(set2)) # (print(set1 - set2))
+(print(set1 - set2))
